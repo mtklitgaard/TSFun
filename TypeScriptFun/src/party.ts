9d09405bcc75714790e8b082a3a6e2b88a49197b
Dr. Klitty <mklittgaard@1800contacts.com>
2015-11-01 15:31:57 -0700

    add party model

---
+++ TypeScriptFun/src/party.ts
@@ -0,0 +1,23 @@
+﻿class Party {
+    private _pimpPoints: number;
+    private _vitalityCost: number;
+    private _partyName: string;
+
+    constructor(pimpPoints: number, vitalityCost: number, partyName: string) {
+        this._pimpPoints = pimpPoints;
+        this._vitalityCost = vitalityCost;
+        this._partyName = partyName;
+    }
+
+    get pimpPoints() {
+        return this.pimpPoints;
+    }
+
+    get vitalityCost() {
+        return this._vitalityCost;
+    }
+
+    get partyName() {
+        return this._partyName;
+    }
+}
